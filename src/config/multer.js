750d3f8d5c9eea317272924a113d8c5ed80050fd
Fabio <fabio12@live.com.br>
2022-01-27 00:52:44 -0300

    config multer

---
+++ src/config/multer.js
@@ -0,0 +1,37 @@
+import multer from "multer";
+import path from "path";
+
+module.exports = {
+  dest: path.resolve(__dirname, '..', '..', 'uploads'),
+  storage: multer.diskStorage({
+    destination: (req, file, cb) {
+      cb(null, path.resolve(__dirname, '..', '..', 'uploads'))
+    },
+    filename: (req, file, cb) => {
+      const uniqueSuffix = new Date.now()
+      cb(null, uniqueSuffix+file.originalname);
+    },
+  }),
+  limits: {
+    fileSize: 2 * 1024 * 1024
+  },
+  fileFilter: (req, file, cb) => {
+    const mimes = [
+      'image/png',
+      'image/jpge',
+      'image/jpg',
+      'image/gif',
+    ];
+
+    /**
+     * Se a img que eu mandar estiver dentro do array mimes
+     * Então eu vou retornar true
+     * Senão vou lançar um erro no callback do FILEFILTER
+     */
+    if (mimes.includes(file.mimetype)) {
+      cb(null, true)
+    }
+    cb(new Error('O formato da imagem é inválido'));
+  }
+
+};
