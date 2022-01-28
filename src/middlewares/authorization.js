ee57cda3cbb8c888663e3df63579a53dc9da3d6d
fabiosdsilva <fabiosds17@gmail.com>
2022-01-28 03:45:38 -0300

    finalizado a autentificacao de rotas

--- src/middlewares/authorization.js
+++ src/middlewares/authorization.js
@@ -1,1 +1,19 @@
-import express from "express";
+import jwt from "jsonwebtoken";
+import User from "../models/User";
+
+export default async (req, res, next) => {
+  const { authorization } = req.headers;
+  if (!authorization) {
+    return res.status(401).json({ errors: ['Token required'] });
+  }
+
+  const [Bearer, token] = authorization.split(" ");
+
+  const payload = await jwt.verify(token, process.env.JWT_TOKEN_SECRET);
+
+  const credential = await User.findOne({ where: { email: token.email } });
+  if (!payload) {
+    return res.status(401).json({ errors: ['Usuário não encontrado'] });
+  }
+  return next();
+};
