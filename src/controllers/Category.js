0475dc486510eb4aa1990d0e497ef738210f11cb
fabiosdsilva <fabiosds17@gmail.com>
2022-02-07 15:59:59 -0300

    add routes update and delete

--- src/controllers/Category.js
+++ src/controllers/Category.js
@@ -1,5 +1,4 @@
 import Category from "../models/Category";
-
 class CategoryController {
   async index(req, res) {
     try {
@@ -23,16 +22,35 @@ class CategoryController {
     }
   }
 
-  // async show(req, res) {
-
-  // }
-
   async update(req, res) {
-
+    const { id } = req.body;
+    try {
+      const isCategory = await Category.findByPk(id);
+      if (!isCategory) {
+        return res.status(400).json({ error: ['Categoria não encontrada'] });
+      }
+      const update = await isCategory.update(req.body);
+      return res.json(update);
+    } catch (error) {
+      return res.status(400).json({ error: error.errors.map((e) => e.message) });
+    }
   }
 
   async delete(req, res) {
-
+    const { id } = req.params;
+    if (!id) {
+      return res.status(400).json({ error: ['Parâmetro inválido'] });
+    }
+    try {
+      const del = await Category.findByPk(id);
+      if (!del) {
+        return res.status(400).json({ error: ['Categoria não encontrada'] });
+      }
+      const destroy = await del.destroy();
+      return res.json({ delete: true });
+    } catch (error) {
+      return res.status(400).json({ error: error.errors.map((e) => e.message) });
+    }
   }
 }
 
