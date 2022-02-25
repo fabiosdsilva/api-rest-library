0576090dcf34c013ba3efecd2d2527dd7de878e5
fabiosdsilva <fabiosds17@gmail.com>
2022-02-24 21:07:48 -0300

    Add const port

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
 import app from './app';
 
-app.listen(process.env.PORT_SERVER, () => console.log(`Está rodando na porta ${process.env.PORT_SERVER}`));
+const port = process.env.PORT_SERVER || 3000;
+
+app.listen(port, () => console.log(`Está rodando na porta ${port}`));
