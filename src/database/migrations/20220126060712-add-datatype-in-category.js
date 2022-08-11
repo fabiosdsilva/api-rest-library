69abe6b35ff178daf6c4f2d8aac8c9e134550bfb
fabiosdsilva <fabiosds17@gmail.com>
2022-08-10 21:49:59 -0300

    wip: Removed dependecy mysql

--- src/database/migrations/20220126060712-add-datatype-in-category.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-  up: async (queryInterface, Sequelize) => queryInterface.changeColumn('categories', 'id', {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-  }),
-  down: async () => {},
-};
